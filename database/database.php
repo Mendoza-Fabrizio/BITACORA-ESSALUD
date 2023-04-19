7b7d1d267a272eabc61138a080dcee0398d752b8
FavuPolar <fmendozas@unsa.edu.pe>
2023-04-18 20:47:21 -0500

    bitacora terminada

--- database/database.php
+++ database/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database
 {
-  private static $dbName = 'essalud';
+  private static $dbName = 'essalud_essi';
   private static $dbHost = 'localhost';
   private static $dbUsername = 'root';
   private static $dbUserPassword = '';
--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <?php
 include_once('database/database.php');
-;
 $fecha = $_POST;
-$header = ["Fecha", "Hora", "CAS", "ESSL/Explota", "Módulo", "Detalle del problema", "Responsable que registra", "Usuario que reporta", "Fecha a soporte_essl", "Fecha soporte mesadeayuda", "N° Caso mesdeayuda", ["Reporte telefonico a..." => "Fecha", "Destino"], ["Reporte por Email a:..." => "Fecha", "Destino"], ["Reporte por Whatsapp a..." => "Fecha", "Destino"], ["Reporte formal a..." => "Fecha", "Destino"]];
+foreach ($_POST as $p) {
+  array_push($fecha, $p);
+}
+$data = [
+  'fecha' => $fecha[0],
+  'CAS' => $fecha[1],
+  'essi_explota' => $fecha[2],
+  'Modulo' => $fecha[3],
+  'detalle' => $fecha[4],
+  'responsable' => $fecha[5],
+  'usuario_reporte' => $fecha[6],
+  'fecha_essi_soporte' => $fecha[7],
+  'fecha_mesa_soporte' => $fecha[8],
+  'num_caso_mesa_ayuda' => $fecha[9],
+  'fecha_reporte_telefono' => $fecha[10],
+  'destino_reporte_telefono' => $fecha[11],
+  'fecha_reporte_email' => $fecha[12],
+  'destino_reporte_email' => $fecha[13],
+  'fecha_reporte_whatsapp' => $fecha[14],
+  'destino_reporte_whatsapp' => $fecha[15],
+  'fecha_reporte_formal' => $fecha[16],
+  'destino_reporte_formal' => $fecha[17]
+];
+$header = ["ID", "Fecha", "Hora", "CAS", "ESSL/Explota", "Módulo", "Detalle del problema", "Responsable que registra", "Usuario que reporta", "Fecha a soporte_essl", "Fecha soporte mesadeayuda", "N° Caso mesdeayuda", ["Reporte telefonico a..." => "Fecha", "Destino"], ["Reporte por Email a:..." => "Fecha", "Destino"], ["Reporte por Whatsapp a..." => "Fecha", "Destino"], ["Reporte formal a..." => "Fecha", "Destino"]];
 $pdo = Database::connect();
-$con = $pdo->query('SELECT CAS FROM bitacora')->fetchAll(PDO::FETCH_COLUMN);
-var_dump($con);
+try {
+  $sql = 'INSERT INTO bitacora(Fecha, CAS,essi_explota,modulo,detalle,responsable,usuario_reporte,fecha_essi_soporte,fecha_mesa_soporte,num_caso_mesa_ayuda,fecha_reporte_telefono,
+  destino_reporte_telefono,fecha_reporte_email,destino_reporte_email,fecha_reporte_whatsapp,destino_reporte_whatsapp,fecha_reporte_formal,destino_reporte_formal) VALUES(:fecha,:cas,:essi_explota,:modulo,:detalle,:responsable,:usuario_reporte,:fecha_essi_soporte,:fecha_mesa_soporte,:num_caso_mesa_ayuda,:fecha_reporte_telefono,
+  :destino_reporte_telefono,:fecha_reporte_email,:destino_reporte_email,:fecha_reporte_whatsapp,:destino_reporte_whatsapp,:fecha_reporte_formal,:destino_reporte_formal)';
+  $pdo->prepare($sql)->execute($data);
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
+
+$con = $pdo->query('SELECT * FROM bitacora')->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -65,18 +96,18 @@ var_dump($con);
       }
       ?>
     </tr>
-    <tr>
+    <?php foreach ($con as $c) {
 
-      <?php
-      foreach ($fecha as $valor) {
+      echo '<tr>';
+      for ($i = 0; $i < count($c) / 2; $i++) {
         echo '<td>
-          <p>' . $valor . '</p>
-          </td>';
+                <p>' . $c[$i] . '</p>
+                </td>';
       }
 
-      ?>
-
-    </tr>
+      echo ' </tr>';
+    }
+    ?>
   </table>
   <button type="button"><a href="formulario.php">Volver</a></button>
 </body>
